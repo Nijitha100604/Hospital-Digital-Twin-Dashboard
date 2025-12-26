51d42fbf857926229084a68cd55e4e5a683ef937
Nijitha100604 <nijitha2020@gmail.com>
2025-12-26 18:12:55 +0530

    updated

---
+++ Frontend/src/assets/assets.js
@@ -0,0 +1,19 @@
+import login_image from './login-image.png'
+import patient_profile_female from './patient_profile_female.jpg'
+import patient_profile_male from './patient_profile_male.jpg'
+import mri from 'MRI.jpg'
+import patient_monitor from './patient_monitor.jpg'
+import ecg from './ECG.webp'
+import ultrasound_machine from './ultrasound-machine.jpg'
+import anesthesia_machine from './anesthesia_machine.avif'
+
+export const assets = {
+    login_image,
+    patient_profile_female,
+    patient_profile_male,
+    mri,
+    patient_monitor,
+    ecg,
+    ultrasound_machine,
+    anesthesia_machine
+}
