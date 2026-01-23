b5e5f5919369cf2538ba501743ad435429881028
vijay-05012004 <vijaykarthick.bm22@bitsathy.ac.in>
2026-01-23 19:41:54 +0530

    updated

--- Backend/routes/staffRoute.js
+++ Backend/routes/staffRoute.js
@@ -1,10 +1,59 @@
-import express from 'express';
 import { addStaff } from '../controllers/staffController.js';
-import upload from '../middlewares/multer.js';
-import authUser from '../middlewares/authUser.js';
+import express from "express";
+import upload from "../middlewares/multer.js";
+import authUser from "../middlewares/authUser.js";
+
+import {
+  addStaff,
+  getAllStaff,
+  getStaffById,
+  updateStaff,
+  deleteStaff,
+} from "../controllers/staffController.js";
 
 const staffRouter = express.Router();
 
-staffRouter.post('/add-staff', authUser, upload.single('image'), addStaff);
+// --- Configuration for Multiple File Uploads ---
+const staffUploads = upload.fields([
+  { name: "profilePhoto", maxCount: 1 },
+  { name: "idProofDoc", maxCount: 1 },
+]);
+
+// Add New Staff
+staffRouter.post(
+  "/add-staff", 
+  authUser, 
+  staffUploads, // Handles both profilePhoto and idProofDoc
+  addStaff
+);
+
+// Get All Staff
+staffRouter.get(
+  "/all-staff", 
+  authUser, 
+  getAllStaff
+);
+
+// Get Staff by ID
+staffRouter.get(
+  "/staff/:id", 
+  authUser, 
+  getStaffById
+);
+
+// Update Staff
+staffRouter.put(
+  "/update/:id", 
+  authUser, 
+  staffUploads, // Allows updating images as well
+  updateStaff
+);
+
+// Delete Staff
+staffRouter.delete(
+  "/delete/:id", 
+  authUser, 
+  deleteStaff
+);
 
 export default staffRouter;
