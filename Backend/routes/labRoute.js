1a10b64cf34023b838cacce7ca4b9f4d76665c7d
vijay-05012004 <vijaykarthick.bm22@bitsathy.ac.in>
2026-02-04 22:25:12 +0530

    updated

--- Backend/routes/labRoute.js
+++ Backend/routes/labRoute.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import authUser from '../middlewares/authUser.js';
+import { 
+    // ... other imports
+    getReportById 
+} from '../controllers/labController.js';
 
 // --- FIX: Import from labController.js, NOT consultationController.js ---
 import { addLabReports, getAllLabReports } from '../controllers/labController.js'; 
@@ -8,5 +12,6 @@ const labReportRouter = express.Router();
 
 labReportRouter.post('/add', authUser, addLabReports);
 labReportRouter.get('/all-reports', authUser, getAllLabReports);
+labReportRouter.get('/:id', authUser, getReportById);
 
 export default labReportRouter;
